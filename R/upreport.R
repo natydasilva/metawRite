06e73f74fca0cfe5384dc536f1d640906c9636a3
Natalia da Silva <ndasilva@iastate.edu>
2017-05-25 16:05:20 -0500

    clean code

--- R/upreport.R
+++ R/upreport.R
@@ -365,8 +365,7 @@ server = function(input, output, session) {
   )
   
   #Make reactive the new information in the report
-  
-  
+
   titleproto <- shiny::reactive({
     list("titleproto",input$titleproto)
   })
@@ -439,8 +438,7 @@ server = function(input, output, session) {
   
   
   #it is the 
-  if(initial==FALSE){
-    
+  if(initial == FALSE){
     shinyjs::show("updateproto")
     output$updateproto <- shiny::renderUI({
       # reactiveFileReader(1000,)
@@ -451,7 +449,6 @@ server = function(input, output, session) {
       shiny::selectInput("updateproto", "Update report", reportnamesproto)
     })
   }else{
-  
   # action to take when a submit another button is pressed
   shiny::observeEvent(input$submit_anotherproto, {
     
@@ -470,6 +467,7 @@ server = function(input, output, session) {
     
   })
   }
+  
   ###############
   #   TAB 2     #
   ###############
@@ -574,24 +572,7 @@ server = function(input, output, session) {
 
     #Timereport <- function() format(Sys.time(), "%Y%m%d-%H%M%OS")
     ccaux <- list("title", "abstract", "introduction", "method", "result", "discussion", "funding")
-# 
-#     saveData <- function(data,cc) {
-#       if(length(data[[2]] > 0)){
-#         if(!file.exists("tools")) system(sprintf("mkdir %s", "tools"))
-# 
-#         fileName <- paste("tools/",Timereport(),data[[1]],".txt", sep="")
-#         fileConn <- file(fileName)
-#         writeLines(data[[2]], fileConn)
-#         close(fileConn)
-#       }else{
-#         fileName <- paste("tools/",Timereport(),cc,".txt", sep="")
-#         fileConn <- file(fileName)
-#         writeLines(input$noquote(cc), fileConn)
-#         close(fileConn)
-# 
-#       }
-# 
-#     }
+
 
   # action to take when submit button is pressed
 
@@ -643,7 +624,6 @@ server = function(input, output, session) {
   })
 
 
-
   if(initial==FALSE){
     shinyjs::show("reportupdate")
  
@@ -677,72 +657,7 @@ server = function(input, output, session) {
   ###############
   #   TAB 4     #
   ###############
-# 
-#   selectedData <- shiny::reactive({
-#     input$goButton
-# 
-#   })
-# 
-#   up <- NULL
-#   sel <- datapair %>% dplyr::filter(up%in% "1") %>% dplyr::select(trt.pair) %>% unique()
-#   output$mytreat <- shiny::renderUI({
-# 
-#     choi <- datapair %>% dplyr::filter(up%in% input$updatelab) %>% dplyr::select(trt.pair) %>% unique()
-#     #browser()
-#     shiny::selectInput(
-#       "treatpair",
-#       "Pairwise comparison:", choices = choi
-# 
-#     )
-#   })
-# 
-#   output$forest2 <- shiny::renderPlot({
-# 
-#     if(selectedData()){
-# 
-#       pardat <- pair_result[[as.numeric(input$updatelab)]]
-#       pair <- names(pardat) %in% input$treatpair
-#       npair <- 1:length(pair)
-# 
-#       metafor::forest(pardat[[npair[pair]]][[2]])
-#     }else{
-#       return(NULL)
-#     }
-#   })
-# 
-# 
-# 
-# 
-# 
-#   output$funel2 <- shiny::renderPlot({
-# 
-#     if(selectedData()){
-#       pardat <- pair_result[[as.numeric(input$updatelab)]]
-# 
-#       pair <-
-#         names(pardat) %in% input$treatpair
-#       npair <- 1:length(pair)
-#       metafor::funnel(pardat[[npair[pair]]][[2]])
-#     }else{
-#       return(NULL)
-#     }
-#   })
-# 
-# 
-#   output$summary2 <- shiny::renderPrint({
-#     if(selectedData()){
-#       pardat <- pair_result[[as.numeric(input$updatelab)]]
-# 
-#       pair <-
-#         names(pardat) %in% input$treatpair
-# 
-#       npair <- 1:length(pair)
-# 
-#       return(print(pardat[[npair[pair]]][[2]]))
-#     }else{
-#       return(NULL)
-#     }
-#   })
+
   
   if(pair){
     shinyjs::show("pairupdate")
@@ -771,7 +686,6 @@ server = function(input, output, session) {
   })
   
   
-  
   output$funel2 <- shiny::renderPlot({
     
     pardat <- shiny::isolate(pair_result %>%
@@ -798,12 +712,7 @@ server = function(input, output, session) {
   if(net){
     shinyjs::show("netupdate")
   }
-    # output$netupdate <- shiny::renderUI({
-    #   
-    # 
-    # })
     
-  
     rv <-
       shiny::reactiveValues(data = data.frame(datapair, fill = logical(length(datapair$id))))
     
