7a3e78c0835d3076acd088909945e4209322343b
Natalia da Silva <ndasilva@iastate.edu>
2017-05-30 13:02:41 -0500

    data

---
+++ R/modstr.R
@@ -0,0 +1,13 @@
+#' The data structure to run the app
+#'
+#' \itemize{
+#' \item{list}{ something}
+#' \item{list}{something}
+
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name  modstr
+#' @usage data(modstr)
+#' @format list
+NULL
