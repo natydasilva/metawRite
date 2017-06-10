1412bf92df3b87824c466e6159f1767ab8402765
Natalia da Silva <ndasilva@iastate.edu>
2017-06-10 15:39:58 -0500

    remove PISCO for protocol and report

--- R/upreport.R
+++ R/upreport.R
@@ -39,61 +39,19 @@ upreport <-
       
      
     }
-    lsr <- list(title='Title: Identify the report as a systematic review, meta-analysis, or both',
-                abstract = 'Structured summary: Provide a structured summary including, as applicable: background; objectives; data sources; study eligibility criteria, participants, and interventions;study appraisal and synthesis methods; results; limitations; conclusions and implications of key findings; systematic review registration number.',
-                introduction = 'Rationale: Describe the rationale for the review in the context of what is already known. Objectives: Provide an explicit statement of questions being addressed with reference to participants, interventions, comparisons, outcomes, and study design (PICOS).',
-                method = 'Protocol and registration: Indicate if a review protocol exists, if and where it can be accessed (e.g., Web address), and, if available, provide registration information including registration number.
-                Eligibility criteria: Specify study characteristics (e.g., PICOS, length of follow-up) and report characteristics (e.g., years considered, language, publication status) used as criteria for eligibility, giving rationale.
-                Information sources: Describe all information sources (e.g., databases with dates of coverage, contact with study authors to identify additional studies) in the search and date last searched.
-                Search: Present full electronic search strategy for at least one database, including any limits used, such that it could be repeated.
-                Study selection: State the process for selecting studies (i.e., screening, eligibility, included in systematic review, and, if applicable, included in the meta-analysis).
-                Data collection process: Describe method of data extraction from reports (e.g., piloted forms, independently, in duplicate) and any processes for obtaining and confirming data from investigators.
-                Data items: List and define all variables for which data were sought (e.g., PICOS, funding sources) and any assumptions and simplifications made.
-                Risk of bias in individual studies: Describe methods used for assessing risk of bias of individual studies (including specification of whether this was done at the study or outcome level), and how this information is to be used in any data synthesis.
-                Summary measures:
-                Synthesis of results: Describe the methods of handling data and combining results of studies, if done, including measures of consistency (e.g., I2) for each meta-analysis.
-                Risk of bias across studies:Specify any assessment of risk of bias that may affect the cumulative evidence (e.g., publication bias, selective reporting within studies).
-                Additional analyses:Describe methods of additional analyses (e.g., sensitivity or subgroup analyses, meta-regression), if done, indicating which were pre-specified.',
-                result = 'Study selection: Give numbers of studies screened, assessed for eligibility, and included in the review, with reasons for exclusions at each stage, ideally with a flow diagram.
-                Study characteristics: For each study, present characteristics for which data were extracted (e.g., study size, PICOS, follow-up period) and provide the citations.
-                Risk of bias within studies: Present data on risk of bias of each study and, if available, any outcome level assessment (see item 12).
-                Results of individual studies: For all outcomes considered (benefits or harms), present, for each study: (a) simple summary data for each intervention group (b) effect estimates and confidence intervals, ideally with a forest plot.
-                Synthesis of results: Present results of each meta-analysis done, including confidence intervals and measures of consistency.
-                Risk of bias across studies: Present results of any assessment of risk of bias across studies (see Item 15).
-                Additional analysis:Give results of additional analyses, if done (e.g., sensitivity or subgroup analyses, meta-regression [see Item 16]).',
-                discussion = 'Summary of evidence: Summarize the main findings including the strength of evidence for each main outcome; consider their relevance to key groups (e.g., healthcare providers, users, and policy makers).
-                Limitations: Discuss limitations at study and outcome level (e.g., risk of bias), and at review-level (e.g., incomplete retrieval of identified research, reporting bias).
-                Conclusions: Provide a general interpretation of the results in the context of other evidence, and implications for future research.',
-                funding = 'Describe sources of funding for the systematic review and other support (e.g., supply of data); role of funders for the systematic review.
-                ')
+
+    lsr <- list(title = '',
+                abstract = '',
+                introduction = '',
+                method = '',
+                result = '',
+                discussion = '',
+                funding = '')
     
-    protocol <- list(titleproto ="Identification: Identify the report as a protocol of a systematic review,  update: if the protocol is for an update of a previous systematic review, identify as such,
-                     contact: Provide name, institutional affiliation, e-mail address of all protocol authors; provide physical mailing address of corresponding author, contributions: Describe contributions of protocol authors and identify the guarantor of the review,
-                     Amendments: If the protocol represents an amendment of a previously completed or published protocol, identify as such and list changes; otherwise, state plan for documenting important protocol amendments,
-                     Support: Indicate sources of financial or other support for the review, Sponsors:Provide name for the review funder and/or sponsor,
-                     Role of sponsor or funder: Describe roles of funder(s), sponsor(s), and/or institution(s), if any, in developing the protocol",
-                     introproto ="Rationale:Describe the rationale for the review in the context of what is already known,
-                     Objective: Provide an explicit statement of the question(s) the review will address with reference to participants, interventions, comparators, and outcomes (PICO)", 
-                     methodproto = "Eligibility criterio: Specify the study characteristics (such as PICO, study design, setting, time frame) and report characteristics (such as years considered, language, publication status) to be used as criteria for eligibility for the review,
-                     Information source: Describe all intended information sources (such as electronic databases, contact with study authors, trial registers or other grey literature sources) with planned dates of coverage,
-                     Search strategy: Present draft of search strategy to be used for at least one electronic database, including planned limits, such that it could be repeated,
-                     Study records: Describe the mechanism(s) that will be used to manage records and data throughout the review,
-                     Selection process: State the process that will be used for selecting studies (such as two independent reviewers) through each phase of the review (that is, screening, eligibility and inclusion in meta-analysis),
-                     Data collection process: Describe planned method of extracting data from reports (such as piloting forms, done independently, in duplicate), any processes for obtaining and confirming data from investigators,
-                     Data items: List and define all variables for which data will be sought (such as PICO items, funding sources), any pre-planned data assumptions and simplifications,
-                     Outcomes and priorization: List and define all outcomes for which data will be sought, including prioritization of main and additional outcomes, with rationale,
-                     Risk of Bias: Describe anticipated methods for assessing risk of bias of individual studies, including whether this will be done at the outcome or study level, or both; state how this information will be used in data synthesis,
-                     Data synthesis: Describe criteria under which study data will be quantitatively synthesised
-                     If data are appropriate for quantitative synthesis, describe planned summary measures, methods of handling data and
-                     methods of combining data from studies, including any planned exploration of consistency (such as I2, Kendalls T) 
-                     Describe any proposed additional analyses (such as sensitivity or subgroup analyses, meta-regression)
-                     If quantitative synthesis is not appropriate, describe the type of summary planned,
-                     Meta-bias(es): Specify any planned assessment of meta-bias(es) (such as publication bias across studies, selective reporting within studies),
-                     Confidence in cumulative evidence: Describe how the strength of the body of evidence will be assessed (such as GRADE)")
-    #
-    # library(plotly)
-    
-    
+    protocol <- list(titleproto ="",
+                     introproto ="", 
+                     methodproto = "")
+
     ui = shiny::fluidPage(
       shinyjs::useShinyjs(),
       #shinyjs::inlineCSS(appCSS),
